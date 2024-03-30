188188789c6d91393cf5483d5b30a663fee3e17d
Sergei Sergienko <divelix1@gmail.com>
2024-03-30 16:48:45 +0300

    feat: Copy button for rotation matrix

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button>
+        <slot name="icon"></slot>
+    </button>
+</template>
+<style scoped>
+button {
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    justify-content: center;
+    padding: 10px;
+    background-color: #282828;
+    border-radius: 20%;
+    border: 1px solid var(--c-divider-dark-1);
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #333;
+}
+
+button:active {
+    background-color: #555;
+}
+</style>
--- src/components/RotMat.vue
+++ src/components/RotMat.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { computed, inject, type Ref } from 'vue';
+import IconCopy from './icons/IconCopy.vue';
+import ActionButton from './ActionButton.vue';
 
 const rotMat: Ref<Array<number>> = inject("rotMat")!
-const precision = 2
+const displayPrecision = 3
+const copyPrecision = 5
 const smallNumber = 0.0000000001
 const isPositiveArr = computed(() => {
     return rotMat.value.map(value => {
@@ -19,25 +22,56 @@ const closeToOneArr = computed(() => {
         return 1 - Math.abs(value) < smallNumber
     })
 })
+
+function copyMat() {
+    const numArr = rotMat.value
+    let result = '[\n';
+    for (let i = 0; i < numArr.length; i++) {
+        if (i % 3 === 0) {
+            if (i !== 0) {
+                result += '],\n';
+            }
+            result += '    [';
+        }
+        if (i % 3 !== 0) {
+            result += ',';
+        }
+        result += numArr[i].toFixed(copyPrecision);
+    }
+    result += ']\n]';
+    navigator.clipboard.writeText(result)
+}
 </script>
 
 <template>
     <div>
         <h2>Rotation Matrix</h2>
-        <table>
-            <tr v-for="(row, rowIndex) in [0, 1, 2]" :key="rowIndex">
-                <td v-for="(col, colIndex) in [0, 1, 2]" :key="colIndex">
-                    <div :class="{ one: closeToOneArr[rowIndex * 3 + colIndex] }">
-                        <span :class="{ invisible: isPositiveArr[rowIndex * 3 + colIndex] }">-</span>
-                        {{ absArr[rowIndex * 3 + colIndex].toFixed(precision) }}
-                    </div>
-                </td>
-            </tr>
-        </table>
+        <div class="mat_container">
+            <table>
+                <tr v-for="(row, rowIndex) in [0, 1, 2]" :key="rowIndex">
+                    <td v-for="(col, colIndex) in [0, 1, 2]" :key="colIndex">
+                        <div :class="{ one: closeToOneArr[rowIndex * 3 + colIndex] }">
+                            <span :class="{ invisible: isPositiveArr[rowIndex * 3 + colIndex] }">-</span>
+                            {{ absArr[rowIndex * 3 + colIndex].toFixed(displayPrecision) }}
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <ActionButton @click.prevent="copyMat">
+                <template #icon>
+                    <IconCopy />
+                </template>
+            </ActionButton>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.mat_container {
+    display: flex;
+    gap: 5px;
+}
+
 .invisible {
     opacity: 0.0;
 }
